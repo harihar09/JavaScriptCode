c195247fb2916351d74c820ba74c4e4660ed1129
harish5058 <hariharsangale@gmail.com>
2023-06-25 14:04:54 +0530

    Objects

--- 02-Fundamentals-Part-2/Objects.js
+++ 02-Fundamentals-Part-2/Objects.js
@@ -1,7 +1,37 @@
-let ObjectInJs = {
+let jonas = {
     firstName: "Adam carry",
-    lastName: "Jonatham hughes",
+    lastName: "Mark hughes",
     friends: ['tom', 'jerry']
 }
 
-console.log(ObjectInJs);
+console.log(jonas);
+// get object property using . operator
+console.log(jonas.firstName, jonas.lastName)
+
+// get object property using [] operator, it can be computed using an expression
+console.log(jonas['firstName'])
+console.log(jonas['lastName']);
+const nameSuffix = 'Name';
+console.log(`FirstName ` + jonas['first' + nameSuffix]);
+console.log("Last Name " + jonas['last' + nameSuffix]);
+
+const returnVal = prompt(`Please select one of the below option 
+ firstName,lastName,friends`);
+console.log(returnVal);
+// this will be undefined as returnVal is not jonas property 
+console.log(jonas.returnVal);
+
+// this will evaluate the expression in runtime and get the value
+console.log(jonas[returnVal]);
+
+jonas.location = 'pune';
+jonas['age'] = 20;
+console.log(jonas);
+// undefined is considered as false value in javascript
+if (jonas[returnVal]) {
+    console.log(jonas[returnVal]);
+} else {
+    console.log('wrong value');
+}
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends and his best friend is ${jonas.friends[0]}`);
