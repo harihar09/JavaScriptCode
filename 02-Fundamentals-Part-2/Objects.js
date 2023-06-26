4ca9779e176a910f9bbd7df9b3920612a53ca5b3
harish5058 <hariharsangale@gmail.com>
2023-06-26 15:40:38 +0530

    objects

--- 02-Fundamentals-Part-2/Objects.js
+++ 02-Fundamentals-Part-2/Objects.js
@@ -1,7 +1,18 @@
 let jonas = {
     firstName: "Adam carry",
     lastName: "Mark hughes",
-    friends: ['tom', 'jerry']
+    friends: ['tom', 'jerry'],
+    birthYear: 1991,
+    age,
+    hasDriverLicense: false,
+    // function expressions in js
+    getAge: function () {
+        return this.age = 2023 - this.birthYear;
+    },
+    // summary function 
+    getSummary: function () {
+        return `${this.firstName} is ${this.age} years old and has ${this.hasDriverLicense ? 'a' : 'no'} license`;
+    }
 }
 
 console.log(jonas);
@@ -35,3 +46,38 @@ if (jonas[returnVal]) {
 }
 
 console.log(`${jonas.firstName} has ${jonas.friends.length} friends and his best friend is ${jonas.friends[0]}`);
+
+console.log('Age ' + jonas.getAge());
+console.log('Jonas age using this.age property : ' + jonas.age);
+console.log(jonas.getSummary())
+
+
+
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 100,
+    height: 3.69,
+
+    calcBMI: function () {
+        return this.BMI = this.mass / this.height ** 2;
+    }
+};
+
+const john2 = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 5.95,
+
+    calcBMI: function () {
+        return this.BMI = this.mass / this.height ** 2;
+    }
+};
+
+//console.log(`${john2.fullName} BMI (${john2.calcBMI()}) is higher than ${mark.fullName} (${mark.calcBMI})`);
+if (mark.calcBMI() > john2.calcBMI()) {
+
+    console.log(`${mark.fullName} BMI (${mark.calcBMI()}) is higher than ${john2.fullName} (${john2.calcBMI()})`);
+} else {
+    console.log(`${john2.fullName} BMI (${john2.calcBMI()}) is higher than ${mark.fullName} (${mark.calcBMI()})`)
+}
+
